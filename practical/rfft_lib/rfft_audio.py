3e5c5ce44b6eb1dc7567ad3c6932ce094078a3c8
dummySatya <dummySatya5@gmail.com>
2024-11-07 16:56:26 +0530

    feat: tried with self imple

--- practical/rfft_lib/rfft_audio.py
+++ practical/rfft_lib/rfft_audio.py
@@ -1,10 +1,41 @@
-import numpy as np
 import torch
 import librosa
 import os
+import math
 
 class AudioFileException(Exception):
     "Audio File Exception"
+
+def fft_real_imag(samples: torch.Tensor):
+    
+    N = samples.shape[0]
+    if N == 1:
+        return samples,torch.zeros_like(samples)
+    
+    even_samples = samples[::2]
+    odd_samples = samples[1::2]
+    
+    Feven_real, Feven_imag = fft_real_imag(even_samples)
+    Fodd_real, Fodd_imag = fft_real_imag(odd_samples)
+    
+    # Calculate real and imaginary parts of the twiddle factors
+    angle = -2 * math.pi * torch.arange(N // 2) / N
+    omega_real = torch.cos(angle)
+    omega_imag = torch.sin(angle)
+    
+    # Initialize the output arrays
+    combined_real = torch.zeros(N)
+    combined_imag = torch.zeros(N)
+    
+    # Calculate the combined real and imaginary parts
+    combined_real[:N//2] = Feven_real + omega_real * Fodd_real - omega_imag * Fodd_imag
+    combined_imag[:N//2] = Feven_imag + omega_real * Fodd_imag + omega_imag * Fodd_real
+    
+    combined_real[N//2:] = Feven_real - omega_real * Fodd_real + omega_imag * Fodd_imag
+    combined_imag[N//2:] = Feven_imag - omega_real * Fodd_imag - omega_imag * Fodd_real
+    
+    return combined_real, combined_imag
+
 
 def load_audio(audio_path):
     try:
@@ -16,7 +47,9 @@ def load_audio(audio_path):
         raise AudioFileException("Audio file not supported or path is incorrect")
 
 def apply_rfft(audio):
-    rfft_mag = torch.fft.rfft(audio)
-    rfft_mag = abs(rfft_mag)
+
+    # real,img = fft_real_imag(audio)
+    # rfft_mag = torch.sqrt(torch.square(real) + torch.square(img))
+    rfft_mag = abs(torch.fft.rfft(audio))
     return rfft_mag
     
